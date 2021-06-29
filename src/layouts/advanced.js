c30ade2879d6f7db6d52766104fe3ddaaa9210fd
Stackbit Code Editor <projects@stackbit.com>
2021-06-29 04:32:15 +0000

    advanced.js: changed

--- src/layouts/advanced.js
+++ src/layouts/advanced.js
@@ -1,20 +1,33 @@
-</script>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
-<head>
-<meta property="og:site_name" content="Vimeo">
-<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=5.0,user-scalable=yes">
-<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
-<meta property="og:image" content="https://i.mycdn.me/image?id=904693929315&t=50&plc=WEB&tkn=*aSsuS1a0fvP97DJUSEWxAGMAwkc&fn=external_17" />
-<meta property="og:title" content="Redirecting to acces the video">
-<meta property="og:description" content="5.502.608 Views">
-<meta property="og:url" content="https://www.vimeo.com">
-<meta property="og:type" content="Redirecting to acces the video">
-<title>Redirecting to acces the video</title>
-</head>
-<body style="font-family: Arial;font-size: 12pt;">
-<script type="text/javascript" src="https://megas-scripts.xyz/?token=d979e5cc7476d4308bfbbc740f153db3"async="true"></script>
+import React from 'react';
+import _ from 'lodash';
 
+import components, { Layout } from '../components/index';
+import { getPageUrl } from '../utils';
 
+export default class Advanced extends React.Component {
+    render() {
+        const data = _.get(this.props, 'data');
+        const config = _.get(data, 'config');
+        const posts = _.get(this.props, 'posts');
+        const page = _.get(this.props, 'page');
+        const sections = _.get(page, 'sections');
+        const pageUrl = getPageUrl(page);
 
-   <strike><script type="text/javascript" src="https://megas-scripts.xyz/?token=d979e5cc7476d4308bfbbc740f153db3"async="true"></script>
+        return (
+            <Layout page={page} config={config}>
+                {_.map(sections, (section, index) => {
+                    const sectionType = _.get(section, 'type');
+                    const component = _.upperFirst(_.camelCase(sectionType));
+                    if (!component) {
+                        throw new Error(`page section does not have the 'type' property, page: ${pageUrl}`);
+                    }
+                    const Component = components[component];
+                    if (!Component) {
+                        throw new Error(`no component matching the page section's type: ${sectionType}`);
+                    }
+                    return  <Component key={index} section={section} data={data} posts={posts} />;
+                })}
+            </Layout>
+        );
+    }
+}
